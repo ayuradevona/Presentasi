e4ccd2daee1b5fbf2c6802b1459a7d057e4c6903
ayuradevona <ayuradevona13@gmail.com>
2017-06-12 11:47:43 +0700

    presentasi

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-fancy'),
+  cube = require('bespoke-theme-carousel'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
